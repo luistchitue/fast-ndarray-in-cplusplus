d6bc44133e9168ed6c41082e1558464580e08ef2
Luis Tchitue <ls04af@gmail.com>
2025-08-14 16:23:14 +0100

    update CMakeLists.txt to use main.cpp for executable and modify build script to set ENTRY_POINT

--- scripts/build.bat
+++ scripts/build.bat
@@ -9,6 +9,7 @@ vcpkg install catch2:x64-windows
 set VCPKG_ROOT="C:\Users\ls04a\OneDrive\Documents\c++ tutorial\vcpkg\installed\x64-windows\share\catch2"
 set MSBUILD="c:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
 set UNIT_TESTS="C:\Users\ls04a\OneDrive\Documents\fast-ndarray-c++\win_build\Debug\fast_nd_array_in_cpp.exe"
+set ENTRY_POINT="C:\Users\ls04a\OneDrive\Documents\fast-ndarray-c++\win_build\Debug\fast_nd_array_in_cpp.exe"
 
 cmake .. -G "Visual Studio 17 2022" ^
 -A x64 -D Catch2_DIR=%VCPKG_ROOT%
